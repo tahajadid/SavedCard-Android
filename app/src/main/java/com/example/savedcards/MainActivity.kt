46d52e6e96a0dd7b04e7783dd33736e52063c46c
Taha Jadid <jadid.taha@waroom.com>
2023-01-17 15:17:08 +0100

    Add Navigation and Fragment setup

--- app/src/main/java/com/example/savedcards/MainActivity.kt
+++ app/src/main/java/com/example/savedcards/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.savedcards
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        lateinit var navController: NavController
+        lateinit var activityInstance: MainActivity
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        activityInstance = this
+
+        navController =
+            Navigation.findNavController(this, R.id.nav_host_fragment)
     }
 }
