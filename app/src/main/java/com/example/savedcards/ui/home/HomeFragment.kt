957f260580d280ea4f3aa3f194aa944f953b860d
Taha Jadid <jadid.taha@waroom.com>
2023-01-17 15:33:28 +0100

    Add HomeFragment setup

---
+++ app/src/main/java/com/example/savedcards/ui/home/HomeFragment.kt
@@ -0,0 +1,34 @@
+package com.example.savedcards.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.savedcards.R
+import com.example.savedcards.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_home,
+            container,
+            false
+        )
+
+        return binding.root
+    }
+}
--- app/src/main/java/com/example/savedcards/ui/splashScreen/SplashFragment.kt
+++ app/src/main/java/com/example/savedcards/ui/splashScreen/SplashFragment.kt
@@ -1,13 +1,17 @@
-package com.example.savedcards.ui
+package com.example.savedcards.ui.splashScreen
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.savedcards.R
+import com.example.savedcards.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
+
+    private lateinit var binding: FragmentSplashBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +22,13 @@ class SplashFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_splash,
+            container,
+            false
+        )
+
+        return binding.root
     }
 }
