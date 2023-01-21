2a875f161370e1f8bebff2a373db7079c62d6cec
Taha Jadid <jadid.taha@waroom.com>
2023-01-22 00:17:09 +0100

    Add error and visa & master card

--- app/src/main/java/com/example/savedcards/MainActivity.kt
+++ app/src/main/java/com/example/savedcards/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.savedcards
 
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
@@ -29,6 +33,28 @@ class MainActivity : AppCompatActivity() {
 
         navController =
             Navigation.findNavController(this, R.id.nav_host_fragment)
+    }
+
+    /**
+     * Clear focus of all EditText views
+     * dispatchTouchEvent
+     */
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (ev?.action == MotionEvent.ACTION_DOWN) {
+            val v = currentFocus
+            if (v is EditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(ev.rawX.toInt(), ev.rawY.toInt())) {
+                    v.clearFocus()
+                    val imm =
+                        getSystemService(android.content.Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0)
+                }
+            }
+        }
+
+        return super.dispatchTouchEvent(ev)
     }
 
 }
--- app/src/main/java/com/example/savedcards/data/CardInfo.kt
+++ app/src/main/java/com/example/savedcards/data/CardInfo.kt
@@ -3,11 +3,13 @@ package com.example.savedcards.data
 import com.google.gson.annotations.SerializedName
 
 data class CardInfo(
+    @SerializedName("name") var name: String? = null,
     @SerializedName("number") var number: String? = null,
     @SerializedName("fullName") var fullName: String? = null,
     @SerializedName("expirationMonth") var expirationMonth: String? = null,
     @SerializedName("expirationYear") var expirationYear: String? = null,
     @SerializedName("cvv") var cvv: String? = null,
     @SerializedName("rib") var rib: String? = null,
-    @SerializedName("themeId") var themeId: String? = null
+    @SerializedName("themeId") var themeId: String? = null,
+    @SerializedName("cardType") var cardType: String? = null
 )
